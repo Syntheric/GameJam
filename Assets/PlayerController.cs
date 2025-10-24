6a86c28a22f7bd94272e19af5c4b5bdc432e4fa3
Syntheric <mrsyntheric@gmail.com>
2025-10-24 21:56:01 +0700

    Second Commit

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 7.0f;
+
+    private Rigidbody2D Rb;
+    private bool isGrounded = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float move = Input.GetAxis("Horizontal");
+        Rb.linearVelocity = new Vector2(move * moveSpeed, Rb.velocity.y);
     }
 }
